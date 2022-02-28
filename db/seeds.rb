82d90e4284998a9b9bcfe2b82b8b92c0252bb6fc
alex hall <alex_hall@hotmail.co.uk>
2022-02-28 17:54:45 +0000

    working on seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,59 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#require
+require 'faker'
+
+puts "Making the bosses!"
+
+['al@boss.com', 'anja@boss.com', 'alex@boss.com'].each do |user|
+  User.create(
+    #name?
+    email: user,
+    password: 'password',
+  )
+
+  puts "Boss #{user} made!"
+end
+
+puts "---------------------------" * 2
+
+puts "Creating Medium Oddbox!"
+
+oddbox = Oddbox.new(name: "Medium Oddbox", ingredients: "Potatoes, Aubergines, Rocket, Spring Onions, Spring Greens, Cauliflower, Mushrooms, Celery")
+
+puts "Creating Medium Oddbox!"
+
+oddbox_ingredients = Ingredients.new()
+
+puts "---------------------------" * 2
+
+puts "Creating Recipes!"
+
+10.times do
+recipes = Recipe.new(
+  name: Faker::Food.dish
+  rating: (0..5).sample
+  description: Faker::Food.description
+  cooking_time: "10 minutes"
+  substitute: Faker::Food.ingredient
+  leftover: Faker::Food.vegetables
+  store_recipe: "1 week"
+)
+  recipes.save
+end
+
+puts "---------------------------" * 2
+
+puts "Creating Reviews"
+
+10.times do
+review = Review.new(
+  content: Faker::Quote.famous_last_words
+  rating: (0..5).sample
+)
+  review.save
+end
+
+puts "---------------------------" * 2
