b316af59cd4efc6b3b0a8998de45016585bb8758
Alastair Turner <specta1989@gmail.com>
2022-02-28 16:32:09 +0000

    Favouritable added to project

--- db/migrate/20220228154644_create_join_table_users_recipes.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableUsersRecipes < ActiveRecord::Migration[6.1]
-  def change
-    create_join_table :users, :recipes do |t|
-      # t.index [:user_id, :recipe_id]
-      # t.index [:recipe_id, :user_id]
-    end
-  end
-end
