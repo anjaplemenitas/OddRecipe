4bce1940de3ba2983f4d575fe8aba79e98f3885e
Alastair Turner <specta1989@gmail.com>
2022-02-28 16:39:37 +0000

    model relationships doneish

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,6 @@
 class Recipe < ApplicationRecord
+  has_many :ingredients, through: :recipes_ingredients
+  has_many :reviews
+
   acts_as_favoritable
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :reviews
+  has_many :recipes, through: :meal_plan
+
   acts_as_favoritor
 end
