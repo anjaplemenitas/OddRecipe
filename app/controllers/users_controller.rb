2217054bf311e0a6f228c4cefbcf13a882f0b9c0
Anja Plemenitas <anja.plemenitas@gmail.com>
2022-03-02 10:38:25 +0000

    controller meal plan

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,4 @@
 class UsersController < ApplicationController
+  def meal_plan
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :oddboxes, only: %i[show]
-  resources :users, only: %i[show]
+  resources :users, only: %i[show] do
+    resources :meal_plans, only: %i[show]
+  end
+
 
   resources :recipes, only: %i[new show edit update destroy] do
     resources :ratings, only: %i[new edit update destroy]
