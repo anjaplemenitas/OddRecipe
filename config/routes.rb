7514edec64fb144f5bebeeae7a6a12d57eb8a2ce
Alastair Turner <specta1989@gmail.com>
2022-03-02 15:03:44 +0000

    Added oddbox scraping to the seed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'user_root', to: redirect("users/:id")
 
   resources :oddboxes, only: %i[show]
   resources :users, only: %i[show]
--- db/seeds.rb
+++ db/seeds.rb
@@ -29,18 +29,6 @@ puts "Creating Medium Oddbox!"
 
 Oddbox.create(name: "Medium Oddbox", user_id: 1)
 
-puts "Creating Medium Oddbox Ingredients!"
-
-100.times do
-  Ingredient.create(name: Faker::Food.vegetables)
-end
-
-10.times do
-  OddboxIngredient.create(oddbox_id: 1, ingredient_id: Ingredient.all.sample.id)
-end
-
-puts "---------------------------" * 2
-
 puts "Creating Recipes!"
 
 odd_url = URI.open("https://www.oddbox.co.uk/recipes")
@@ -51,17 +39,53 @@ urls = odd_noko.search(".curated-posts-section__post a").map do |link|
   link["href"] if link["href"].include?("recipes")
 end
 
-10.times do
-  recipes = Recipe.new(
-    name: Faker::Food.dish,
-    description: Faker::Food.description,
-    cooking_time: "10 minutes",
+urls.compact!
+
+descriptions = []
+method = []
+ingredients = []
+
+urls.each do |url|
+  temp_noko = Nokogiri::HTML(URI.open("https://www.oddbox.co.uk#{url}"))
+  temp_ingredients = temp_noko.search(".recipe__ingredients p").map do |ing|
+    x = ing.text.match(/^\w?\d*\W?\s*(g|ml|tbsp|tsp)?\s*(?<ing>[a-zA-Z][a-z]*\s?[a-zA-Z|&]*\s?[a-zA-Z]*)/)
+    x['ing'].strip unless x['ing'].strip.nil?
+  end
+
+  ingredients << temp_ingredients
+  descriptions << temp_noko.search(".post__description p").text
+
+  temp_method = temp_noko.search(".recipe__content li").map(&:text)
+
+  method << temp_method
+end
+
+ind_ing = ingredients.join(", ").split(", ").sort.uniq
+
+ind_ing.each do |ing|
+  Ingredient.create(name: ing)
+end
+
+puts "Creating Medium Oddbox Ingredients!"
+
+["Avocado", "Beetroots", "Bell peppers", "Brussels sprouts", "Carrots", "Cucumbers", "Ginger", "Limes"].each do |ing|
+  OddboxIngredient.create(oddbox_id: 1, ingredient_id: Ingredient.find_by(name: ing).id)
+end
+
+puts "---------------------------" * 2
+
+names.each_with_index do |name, index|
+  recipe = Recipe.new(
+    name: name,
+    description: descriptions[index],
+    cooking_time: [10, 15, 20, 25, 30, 35, 40].sample,
     leftover: Faker::Food.vegetables,
     storage: "1 week"
   )
-  recipes.save
-  5.times do
-    RecipeIngredient.create(recipe_id: recipes.id, ingredient_id: Ingredient.all.sample.id)
+  recipe.save
+
+  ingredients[index].each do |ing|
+    RecipeIngredient.create(recipe_id: recipe.id, ingredient_id: Ingredient.find_by(name: ing).id)
   end
 end
 
