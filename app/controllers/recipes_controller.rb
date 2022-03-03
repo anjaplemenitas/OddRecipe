fc3f58226a2c83af5afda71c0d162de112e1352e
Alastair Turner <specta1989@gmail.com>
2022-03-03 17:26:46 +0000

    WIP

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -14,4 +14,22 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
   end
 
+  def update
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(recipe_params)
+  end
+
+  def like
+    @recipe = Recipe.find(params[:id])
+
+    @recipe.favorited_by?(current_user) ? current_user.unfavorite(@recipe) : current_user.favorite(@recipe)
+
+    redirect_to recipe_path(@recipe)
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :cooking_time, :description, :method, :leftover, :storage, :substitute, :image_url)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,14 @@
 class UsersController < ApplicationController
+  def show
+    @oddbox = current_user.oddbox
+  end
 
   def meal_plans
     @user = current_user
     @meal_plan = MealPlan.where(user_id: @user.id)
   end
 
-  def show
-    @oddbox = current_user.oddbox
+  def oddbook
+    @oddbook = current_user.all_favorited
   end
 end
