167d06c10756ba80fca3a811f3e458407fd54b17
Alastair Turner <specta1989@gmail.com>
2022-03-04 08:10:29 +0000

    Can create mealplan from calendar

--- app/controllers/meal_plans_controller.rb
+++ app/controllers/meal_plans_controller.rb
@@ -1,15 +1,20 @@
 class MealPlansController < ApplicationController
-
   def create
     @meal_plan = MealPlan.new
     @recipe = Recipe.find(params[:recipe_id])
-    @meal_plan.recipe = @recipe
-    @meal_plan.user = current_user
+    @meal_plan.recipe_id = @recipe.id
+    @meal_plan.user_id = current_user.id
+    @meal_plan.date = Date.new(2022, 3, params["date"].to_i)
     if @meal_plan.save
-      redirect_to user_path(current_user), notice: "Your meal has been added"
+      redirect_to user_path(current_user), notice: "#{@recipe.name} has been added"
     else
       render 'recipes/show'
     end
   end
 
+  private
+
+  def mealplan_params
+    params.require(:meal_plan).permit(:date, :day, :recipe_id)
+  end
 end
