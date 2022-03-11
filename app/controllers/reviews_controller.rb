ae65c68197718fae770e0cbab40d7421a01a3aa3
Alastair Turner <specta1989@gmail.com>
2022-03-11 08:54:14 +0000

    Fixed empty review crash

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -10,6 +10,8 @@ class ReviewsController < ApplicationController
     @review.user_id = current_user.id
     @review.recipe_id = @recipe.id
 
+    empty_review?(@review)
+
     if @review.save
       redirect_to new_recipe_review_path(@recipe), notice: "Review added"
     else
@@ -18,6 +20,11 @@ class ReviewsController < ApplicationController
   end
 
   private
+
+  def empty_review?(review)
+    @review.rating = 0 if review.rating.nil?
+    @review.content = "#{current_user.first_name} did not leave a review." if review.content.empty?
+  end
 
   def review_params
     params.require(:review).permit(:content, :rating)
